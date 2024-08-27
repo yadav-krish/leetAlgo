a0a4070bb242547942e4d7dc2bb608b4a060db0e
Krishna Yadav <140491069+yadav-krish@users.noreply.github.com>
2024-08-28 00:07:23 +0530

    Create Longest Palindromic Substring.java

---
+++ TUF/Strings/Longest Palindromic Substring.java
@@ -0,0 +1,43 @@
+// Given a string s, return the longest 
+// palindromic
+ 
+// substring
+//  in s.
+
+ 
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: "bb"
+
+class Solution {
+    public String longestPalindrome(String s) {
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);         // Odd length palindromes
+            int len2 = expandAroundCenter(s, i, i + 1);     // Even length palindromes
+            int len = Math.max(len1, len2);
+
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+     private static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
