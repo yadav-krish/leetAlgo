95de9e1e208b0795807b092691bbda3055823022
Krishna Yadav <140491069+yadav-krish@users.noreply.github.com>
2024-08-30 23:20:42 +0530

    Update Reverse Linked List.java

--- TUF/LL/Reverse Linked List.java
+++ TUF/LL/Reverse Linked List.java
@@ -7,7 +7,9 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
+//  */
+
+// Iterative Approach
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev=null;
@@ -21,5 +23,19 @@ class Solution {
             curr=next;
         }
         return prev;
+    }
+// }
+
+// Recursive Approach
+    class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null||head.next==null)
+        return head;
+        ListNode newHead=reverseList(head.next);
+        ListNode front=head.next;
+        front.next=head;
+        head.next=null;
+        return newHead;
+        
     }
 }
