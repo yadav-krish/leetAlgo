73aa627e5671b133b1c3a6265b346c7554c4e3d3
Krishna Yadav <140491069+yadav-krish@users.noreply.github.com>
2024-09-05 23:38:09 +0530

    Update Intersection of Two Linked Lists.java

--- TUF/LL/Intersection of Two Linked Lists.java
+++ TUF/LL/Intersection of Two Linked Lists.java
@@ -17,6 +17,40 @@
  *     }
  * }
  */
+
+//Brute
+
+//Time Complexity : O(m * n)
+//Space Complexity : O(1)
+
+public class Solution {
+   
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp;
+
+        // Iterate through the nodes of the first linked list
+        while (headA != null) {
+            temp = headB;
+
+            // Iterate through the nodes of the second linked list
+            while (temp != null) {
+                // If the current nodes are the same, we found the intersection
+                if (headA == temp) {
+                    return headA;
+                }
+                temp = temp.next;
+            }
+
+            // Move to the next node in the first linked list
+            headA = headA.next;
+        }
+
+        // No intersection found
+        return null;
+    }
+}
+
+// Approach 2
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
        HashSet<ListNode>set=new HashSet<>();
