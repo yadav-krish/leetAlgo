fe268691606c064a046e1289edf77dde92bdf6f4
Krishna Yadav <140491069+yadav-krish@users.noreply.github.com>
2024-11-14 23:56:38 +0530

    Create Implement Min Stack.java

---
+++ TUF/Stacks and Queue/Implement Min Stack.java
@@ -0,0 +1,81 @@
+// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+// Implement the MinStack class:
+
+// MinStack() initializes the stack object.
+// void push(int val) pushes the element val onto the stack.
+// void pop() removes the element on the top of the stack.
+// int top() gets the top element of the stack.
+// int getMin() retrieves the minimum element in the stack.
+// You must implement a solution with O(1) time complexity for each function.
+
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        // Push the minimum value to minStack
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        // If the popped element is the current minimum, pop it from minStack as well
+        if (stack.pop().equals(minStack.peek())) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+// Approach 2
+
+class Pair {
+    int x, y;
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+class MinStack {
+    Stack < Pair > st;
+    public MinStack() {
+        st = new Stack < > ();
+    }
+
+    public void push(int x) {
+        int min;
+        if (st.isEmpty()) {
+            min = x;
+        } else {
+            min = Math.min(st.peek().y, x);
+        }
+        st.push(new Pair(x, min));
+    }
+
+    public void pop() {
+        st.pop();
+    }
+
+    public int top() {
+        return st.peek().x;
+    }
+
+    public int getMin() {
+        return st.peek().y;
+    }
+}
