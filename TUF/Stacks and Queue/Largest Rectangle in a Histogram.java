48a5b5e635af45ded103186605ea1ff731355b23
Krishna Yadav <140491069+yadav-krish@users.noreply.github.com>
2024-11-26 19:48:48 +0530

    Update Largest Rectangle in a Histogram.java

--- TUF/Stacks and Queue/Largest Rectangle in a Histogram.java
+++ TUF/Stacks and Queue/Largest Rectangle in a Histogram.java
@@ -40,3 +40,29 @@ class Solution {
         return maxi;
     }
 }
+
+// Appraoch 2
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n=heights.length;
+        Stack<Integer>stack=new Stack<>();
+        int maxA=0;
+        for(int i=0;i<=n;i++)
+        {
+            while(!stack.isEmpty()&&(i==n||heights[stack.peek()]>=heights[i]))
+            {
+                int h=heights[stack.peek()];
+                stack.pop();
+                int width;
+                if(stack.isEmpty())
+                width=i;
+                else
+                width=i-stack.peek()-1;
+                maxA=Math.max(maxA,width*h);
+            }
+            stack.push(i);
+        }
+        return maxA;
+    }
+}
